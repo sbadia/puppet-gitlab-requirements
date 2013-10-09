21efd6d30618d55625afbcfcb2859864ac75b276
Andrew Tomaka <atomaka@gmail.com>
2013-10-09 16:49:46 -0400

    Add dependencies to project
    Currently using this as the code for a module while testing with
    vagrant-gitlab.

--- manifests/init.pp
+++ manifests/init.pp
@@ -9,10 +9,34 @@
 # Class:: gitlab_requirements
 #
 #
-class gitlab_requirements {
-  include 'ssh'
-  include 'redis'
-  include 'git'
+class depends($gitlab_dbname, $gitlab_dbuser, $gitlab_dbpwd) {
+  include redis
+  include nginx
+  include mysql::server
 
+  class { 'ruby':
+    version         => '1:1.9.3',
+    rubygems_update => false;
+  }
 
-} # Class:: gitlab_requirements
+  class { 'ruby::dev': }
+
+  mysql::db {
+    $gitlab_dbname:
+      ensure   => 'present',
+      charset  => 'utf8',
+      user     => $gitlab_dbuser,
+      password => $gitlab_dbpwd,
+      host     => 'localhost',
+      grant    => ['all'],
+      # See http://projects.puppetlabs.com/issues/17802 (thanks Elliot)
+  }
+
+  anchor { 'depends::begin': }
+  anchor { 'depends::end': }
+
+  Anchor['depends::begin'] ->
+  Class['redis'] -> Class['nginx'] -> Class['ruby'] -> Class['ruby::dev'] ->
+  Class['mysql::server'] -> Mysql::Db[$gitlab_dbname] ->
+  Anchor['depends::end']
+}
